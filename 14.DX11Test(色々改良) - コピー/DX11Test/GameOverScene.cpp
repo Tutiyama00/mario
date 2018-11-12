c871299913fe03a1adad05e2049317a32c3ab883
KouheiTutiyama <35854995+Tutiyama00@users.noreply.github.com>
2018-11-12 14:28:56 +0900

    コメント追加

--- 14.DX11Test(色々改良) - コピー/DX11Test/GameOverScene.cpp
+++ 14.DX11Test(色々改良) - コピー/DX11Test/GameOverScene.cpp
@@ -8,6 +8,10 @@
 
 using namespace OriginalMath;
 
+/// <summary>
+/// コンストラクタ
+/// </summary>
+/// <param name="pDevice"></param>
 GameOverScene::GameOverScene(ID3D11Device* pDevice)
 {
 	m_NextGameState = GameState::GAMEOVER;
@@ -20,12 +24,21 @@ GameOverScene::GameOverScene(ID3D11Device* pDevice)
 	m_pTextCharacters = new TextCharacters(pos, size, "GAME OVER", pDevice, TPS_WHITE);
 }
 
+/// <summary>
+/// デストラクタ
+/// </summary>
 GameOverScene::~GameOverScene()
 {
 	if (m_pCamera         != nullptr) { delete m_pCamera;         m_pCamera = nullptr; }
 	if (m_pTextCharacters != nullptr) { delete m_pTextCharacters; m_pTextCharacters = nullptr; }
 }
 
+/// <summary>
+/// シーンの更新
+/// </summary>
+/// <param name="inputFlag"></param>
+/// <param name="pDx11"></param>
+/// <returns></returns>
 GameState GameOverScene::UpDateScene(InputFlag inputFlag, Dx11* pDx11)
 {
 	m_NextGameState = GameState::GAMEOVER;
@@ -36,6 +49,11 @@ GameState GameOverScene::UpDateScene(InputFlag inputFlag, Dx11* pDx11)
 	return m_NextGameState;
 }
 
+/// <summary>
+/// ゲームの更新
+/// </summary>
+/// <param name="inputFlag"></param>
+/// <param name="pDevice"></param>
 void GameOverScene::UpDateGame(InputFlag inputFlag, ID3D11Device* pDevice)
 {
 	if (inputFlag.Check(InputFlagCode::INPUT_RIGHT))
@@ -44,6 +62,10 @@ void GameOverScene::UpDateGame(InputFlag inputFlag, ID3D11Device* pDevice)
 	}
 }
 
+/// <summary>
+/// 描画
+/// </summary>
+/// <param name="pDx11"></param>
 void GameOverScene::Draw(Dx11* pDx11)
 {
 	m_pCamera->Shoot(pDx11->m_pDeviceContext, &pDx11->GetViewPort(), 0.0f);
