4b2fca366c5f189b750dcc118f9360ce09fcf883
KouheiTutiyama <35854995+Tutiyama00@users.noreply.github.com>
2018-11-19 11:07:43 +0900

    調整

--- 14.DX11Test(色々改良) - コピー/DX11Test/Kuribo.h
+++ 14.DX11Test(色々改良) - コピー/DX11Test/Kuribo.h
@@ -1,12 +1,16 @@
 #pragma once
 
 #include"Enemy.h"
+#include"IMoveObj.h"
 
-class Kuribo : public Enemy
+class Kuribo : public Enemy, public IMoveObj
 {
 public:
 	Kuribo(Vector3 pos, Vector2 size);
 	~Kuribo() {};
+
+	/*---IMoveObj---*/
+	void Move(InputFlag* inputFlag);
 
 protected:
 	/*---Enemy---*/
